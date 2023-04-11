f0d0eca3e7ec653c450f2a82898668cdc00f2e89
Victor Gimenez <victor.ddr.zn@gmail.com>
2023-04-10 22:50:11 -0300

    Código e documentação finalizados

--- Code.java
+++ Code.java
@@ -1,5 +1,6 @@
 package Main;
 
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Scanner;
 
@@ -8,10 +9,11 @@ public class DescriptografiaAlternativa {
 	
 		Scanner sc = new Scanner(System.in);
 		Integer loop = 1;
-		String[] arrayCripto = {};
+		ArrayList<Object> arrayCripto = new ArrayList<Object>();
 		String messagesCripto = "";
         
 		for(int i = 0; i < loop;){
+			messagesCripto = "";
 
 			System.out.println("Introduza uma frase para ser criptografada:");
 			String textoPlano = sc.nextLine();
@@ -21,10 +23,12 @@ public class DescriptografiaAlternativa {
 			Thread.sleep(1000);
 			System.out.println("Texto criptografado: " + cripto);
 			
-			arrayCripto = new String[]{cripto};
-			for(int j = 0; j < arrayCripto.length;j++) {
-				messagesCripto += "\n " + arrayCripto[j];
+			arrayCripto.add(cripto);
+			for(int j = 0; j < arrayCripto.size();j++) {
+				int h = j + 1;
+				messagesCripto += "\nTexto número "+ h + " -> " + arrayCripto.get(j) + ",";
 			}
+			messagesCripto = messagesCripto.substring(0, messagesCripto.length() - 1);
 			
 			System.out.println();
 			Thread.sleep(1000);
@@ -37,9 +41,12 @@ public class DescriptografiaAlternativa {
 				String textoDescripto = new String(byteDescripto);
 			
 				System.out.println();
-				System.out.println("Texto descriptografo: " + textoDescripto);
+				Thread.sleep(1000);
+				System.out.println("Texto descriptografado: " + textoDescripto);
 			}
 			
+			System.out.println();
+			Thread.sleep(1000);
 			System.out.println("Deseja visualizar as mensagens anteriormente criptografadas? (S/N)");
 			question = sc.next().charAt(0);
 			sc.nextLine();
@@ -47,9 +54,12 @@ public class DescriptografiaAlternativa {
 			if (question == 'S') {
 				
 				System.out.println();
+				Thread.sleep(1000);
 				System.out.println("Textos descriptografados anteriormente: " + messagesCripto);
 			}
 			
+			System.out.println();
+			Thread.sleep(1000);
 			System.out.println("Caso deseje criptografar uma nova mensagem, basta digitar \"Sim\", caso contrário digite \"Não\". ");
 			String answerLoop = sc.nextLine();
 			
